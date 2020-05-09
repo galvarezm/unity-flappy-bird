270773838c2b86430940785182d0d0783ec205e1
Gabriel Alvarez <galvarez.chile@gmail.com>
2020-05-09 02:36:51 -0400

    Mejoras con la vista de la camara y mejores colisiones con el personaje.

--- Assets/Scripts/GeneratorObstacleScript.cs
+++ Assets/Scripts/GeneratorObstacleScript.cs
@@ -9,13 +9,14 @@ public class GeneratorObstacleScript : MonoBehaviour
     private float initialTime = 0;
     public GameObject objObstacle;
     public float highObstables;
+    public int secondsToDestroy;
 
     // Start is called before the first frame update
     void Start()
     {
         GameObject newObjObstacle = Instantiate(objObstacle);
         newObjObstacle.transform.position = transform.position + new Vector3(0,0,0);
-        Destroy(newObjObstacle, 5);
+        Destroy(newObjObstacle, secondsToDestroy);
     }
 
     // Update is called once per frame
@@ -25,7 +26,7 @@ public class GeneratorObstacleScript : MonoBehaviour
             initialTime = 0;
             GameObject newObjObstacle = Instantiate(objObstacle);
             newObjObstacle.transform.position = transform.position + new Vector3(0, Random.Range(-highObstables,highObstables),0);
-            Destroy(newObjObstacle, 5);
+            Destroy(newObjObstacle, secondsToDestroy);
         }else{
             initialTime += Time.deltaTime;
         }
